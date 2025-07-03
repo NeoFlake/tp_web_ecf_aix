643d9f9cef00368cf568f7959c86f10533a706a5
Jonathan <jonathan.schmidt.13@gmail.com>
2025-07-03 14:21:09 +0200

    Nom/Prénom de l'admin connecté apparaît

--- views/partials/_navbar.php
+++ views/partials/_navbar.php
@@ -1,12 +1,13 @@
 <?php
 
 $book_search = $_SESSION["book_search"] ?? null;
+$admin_logged = $_SESSION["admin_logged"];
 
 unset($_SESSION["book_search"]);
 
 ?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -22,16 +23,19 @@ unset($_SESSION["book_search"]);
                     </li>
                     <li class="nav-item">
                         <form action="../src/controllers/connection_controller.php" method="post">
-                            <button type="submit" class="btn btn-outline-primary btn-sm mt-1" name="deconnection" value="true" >Déconnexion</button>
+                            <button type="submit" class="btn btn-outline-primary btn-sm mt-1" name="deconnection" value="true">Déconnexion</button>
                         </form>
                     </li>
+                    <span class="navbar-text ms-3">
+                        <?php echo $admin_logged["last_name"] . " " . $admin_logged["first_name"] ?>
+                    </span>
                 <?php } ?>
             </ul>
             <?php if (isset($_SESSION["main_page_displayed"])) { ?>
-            <form class="d-flex" role="search" action="../src/controllers/book_controller.php" method="post">
-                <input class="form-control me-2" type="search" placeholder="cherchez votre livre..." aria-label="Search" name="book_search" value="<?php echo isset($book_search) ? $book_search : null ?>">
-                <button class="btn btn-outline-success" type="submit" name="book_search_validation" value="validated" >Rechercher</button>
-            </form>
+                <form class="d-flex" role="search" action="../src/controllers/book_controller.php" method="post">
+                    <input class="form-control me-2" type="search" placeholder="cherchez votre livre..." aria-label="Search" name="book_search" value="<?php echo isset($book_search) ? $book_search : null ?>">
+                    <button class="btn btn-outline-success" type="submit" name="book_search_validation" value="validated">Rechercher</button>
+                </form>
             <?php } ?>
         </div>
     </div>
