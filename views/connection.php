8fcfbf6a4efac98df9976db9cac9c0a8e20d9a3d
Jonathan <jonathan.schmidt.13@gmail.com>
2025-07-03 13:31:32 +0200

    Création formulaire connexion

--- views/connection.php
+++ views/connection.php
@@ -2,7 +2,9 @@
 
 session_start();
 
-unset($_SESSION["main_page_displayed"]);
+$email = $_SESSION["email"] ?? null;
+
+unset($_SESSION["main_page_displayed"])
 
 ?>
 
@@ -19,6 +21,28 @@ unset($_SESSION["main_page_displayed"]);
 <body>
     <div class="container">
         <?php include __DIR__ . "/partials/_navbar.php" ?>
+        <div class="d-flex justify-content-center">
+            <h1>Connexion</h1>
+        </div>
+        <div class="d-flex justify-content-center mt-5">
+            <form action="../../src/controllers/user/user_controller.php" method="post">
+                <div class="row">
+                    <div class="col-12">
+                        <label for="email" class="col-form-label">Email </label>
+                        <input type="email" id="email" class="form-control" name="email" value="<?php echo isset($email) ? $email : null ?>" />
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <label for="password" class="col-form-label">Mot de passe</label>
+                        <input type="password" id="password" class="form-control" name="password" />
+                    </div>
+                </div>
+                <div class="row mt-5 d-flex justify-content-center">
+                    <button class="btn btn-primary col-10">Connexion</button>
+                </div>
+            </form>
+        </div>
     </div>
     <script src="../utils/bootstrap.bundle.min.js"></script>
 </body>
--- views/gestion_admin.php
+++ views/gestion_admin.php
@@ -20,6 +20,24 @@ $booklist = $_SESSION["booklist"] ?? null;
     <div class="container">
         <?php include __DIR__ . "/partials/_navbar.php" ?>
     </div>
+    <div class="row">
+        Coucou
+        <form action="../src/controllers/connexion_controller.php">
+            <div>
+                <label for="inputPassword5" class="form-label">Password</label>
+                <input type="password" id="inputPassword5" class="form-control" aria-labelledby="passwordHelpBlock">
+                <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </div>
+            </div>
+            <div class="col-auto">
+                <label for="inputPassword6" class="col-form-label">Password</label>
+            </div>
+            <div class="col-auto">
+                <input type="password" id="inputPassword6" class="form-control" aria-labelledby="passwordHelpInline">
+            </div>
+        </form>
+    </div>
     <script src="../utils/bootstrap.bundle.min.js"></script>
 </body>
 
